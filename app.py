2a7fa03f4cc825adc894dff56a23224f4f47242d
Abhinav Chopra <abhinavchopra4407@gmail.com>
2026-04-30 22:11:19 +0530

    Removed plotly image export (kaleido cloud fix)

--- app.py
+++ app.py
@@ -12,10 +12,7 @@ from datetime import datetime
 import random
 import yagmail  
 import re
-import email
-def is_valid_email(email):
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return re.match(pattern, email) is not None
+
 
 def generate_otp():
     return str(random.randint(100000, 999999))
